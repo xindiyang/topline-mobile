c868428e7c83480a135c2176a0fe6f6d43dc0e3d
xindiyang <2425509077@qq.com>
2019-07-09 19:30:46 +0800

    登录页面-布局完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
 // vant官方提供了四种方式引入组件，我们初期开发直接导入所有的组件
+import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="loginWrap">
+    <van-nav-bar
+      title="登录"
+    />
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        label="用户名"
+        left-icon="contact"
+        placeholder="请输入用户名"
+      />
+      <van-field
+        v-model="user.code"
+        type="password"
+        label="密码"
+        left-icon="closed-eye"
+        placeholder="请输入验证码"
+      >
+         <van-button slot="button" round size="small" type="primary">获取验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="btn">
+      <van-button type="info" round size="large">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content{
+  padding: 10px;
+}
+.van-cell-group {
+  background: #fff
+}
+.van-nav-bar{
+  background: skyblue;
+  margin-bottom: 15px;
+}
+.btn{
+  padding: 20px;
+  margin-top: 25px;
+}
+</style>
