4730d58b4922ec4085b4b049ff6cea4b3fc2cd24
xindiyang <2425509077@qq.com>
2019-07-09 15:32:27 +0800

    配置rem适配

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router/index.js'
 import store from './store/index.js'
 // vant官方提供了四种方式引入组件，我们初期开发直接导入所有的组件
 import 'vant/lib/index.css'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
