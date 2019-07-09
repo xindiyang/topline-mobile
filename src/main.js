004a1ac40e0de031c1eda42d8294aa67ec78992a
xindiyang <2425509077@qq.com>
2019-07-09 15:25:22 +0800

    引入组件vant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index.js'
+import store from './store/index.js'
+// vant官方提供了四种方式引入组件，我们初期开发直接导入所有的组件
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
 
